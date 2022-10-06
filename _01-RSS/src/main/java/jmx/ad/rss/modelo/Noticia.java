9954d09ca8ff876b614f1d92e72346940c55c78e
Juanma <jmmorenod@iesch.org>
2022-10-06 12:21:20 +0200

    1 RSS DocumentBuilder

---
+++ _01-RSS/src/main/java/jmx/ad/rss/modelo/Noticia.java
@@ -0,0 +1,17 @@
+package jmx.ad.rss.modelo;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Noticia {
+
+    private String titulo;
+    private String link;
+    private String descripcion;
+    private String contenido;
+    private String fecha;
+    private String imagen;
+}
